447a2dc14ab1b176d4b0b15cc06aba54f54a83dd
David Becker <dasmau89@gmail.com>
2015-10-28 17:30:41 +0100

    open login dialog after client start

--- bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/ApplicationWorkbenchWindowAdvisor.java
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/ApplicationWorkbenchWindowAdvisor.java
@@ -1,26 +1,36 @@
 package de.afbb.bibo.ui;
 
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
+import de.afbb.bibo.ui.dialog.LoginDialog;
+
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
-    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        super(configurer);
-    }
+	public ApplicationWorkbenchWindowAdvisor(final IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
 
-    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
-        return new ApplicationActionBarAdvisor(configurer);
-    }
-    
-    public void preWindowOpen() {
-        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(600, 400));
-        configurer.setShowCoolBar(true);
-        configurer.setShowStatusLine(false);
-    }
-    
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(final IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	@Override
+	public void preWindowOpen() {
+		final IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(600, 400));
+		configurer.setShowCoolBar(true);
+		configurer.setShowStatusLine(false);
+	}
+
+	@Override
+	public void postWindowOpen() {
+		new LoginDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell()).open();
+	}
+
 }
