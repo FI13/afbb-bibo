55d635b8873e5aa28c1bcc61eedf0e1974329edf
David Becker <dasmau89@gmail.com>
2015-10-29 19:43:53 +0100

    added stub for new curator dialog

---
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/dialog/CreateCuratorDialog.java
@@ -0,0 +1,72 @@
+package de.afbb.bibo.ui.dialog;
+
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import de.afbb.bibo.databinding.BindingHelper;
+import de.afbb.bibo.share.model.Curator;
+
+/**
+ * dialog that creates a new instance of type {@link Curator}
+ * 
+ * @author dbecker
+ */
+public class CreateCuratorDialog extends TitleAreaDialog {
+
+	private Text txtName;
+	private Text txtPassword;
+	private Text txtPassword2;
+	private final Curator curator = new Curator();
+	private final DataBindingContext bindingContext = new DataBindingContext();
+
+	public CreateCuratorDialog(final Shell parentShell) {
+		super(parentShell);
+	}
+
+	@Override
+	protected Control createDialogArea(final Composite parent) {
+		final Composite area = (Composite) super.createDialogArea(parent);
+		final Composite container = new Composite(area, SWT.NONE);
+		container.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true));
+		final GridLayout layout = new GridLayout(2, false);
+		layout.marginWidth = 30;
+		layout.horizontalSpacing = 10;
+		container.setLayout(layout);
+
+		final Label lblName = new Label(container, SWT.NONE);
+		lblName.setText("Name");
+		txtName = new Text(container, SWT.BORDER);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(txtName);
+
+		final Label lblPassword = new Label(container, SWT.NONE);
+		lblPassword.setText("Passwort");
+		txtPassword = new Text(container, SWT.BORDER | SWT.PASSWORD);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(txtPassword);
+
+		final Label lblPassword2 = new Label(container, SWT.NONE);
+		lblPassword2.setText("Passwort (Wiederholung)");
+		txtPassword2 = new Text(container, SWT.BORDER | SWT.PASSWORD);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(txtPassword2);
+
+		createBinding();
+
+		createBinding();
+
+		return area;
+	}
+
+	private void createBinding() {
+		BindingHelper.bindStringToTextField(txtName, curator, Curator.class, Curator.FIELD_NAME, bindingContext);
+		BindingHelper.bindStringToTextField(txtPassword, curator, Curator.class, Curator.FIELD_PASSWORD, bindingContext);
+	}
+
+}
