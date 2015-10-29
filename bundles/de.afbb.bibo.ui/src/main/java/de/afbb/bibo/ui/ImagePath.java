bcdda6fa49946d65039fde6054b583d139a7a734
David Becker <dasmau89@gmail.com>
2015-10-29 10:02:54 +0100

    constant holder class for images

---
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/ImagePath.java
@@ -0,0 +1,15 @@
+package de.afbb.bibo.ui;
+
+/**
+ * class to hold constants for image paths
+ * 
+ * @author dbecker
+ */
+public class ImagePath {
+
+	public static final String ICON_SAPMLE = "icons/sample.gif";//$NON-NLS-1$
+	public static final String ICON_SAPMLE2 = "icons/sample2.gif";//$NON-NLS-1$
+	public static final String ICON_SAPMLE3 = "icons/sample3.gif";//$NON-NLS-1$
+
+	public static final String[] images = new String[] {ICON_SAPMLE, ICON_SAPMLE2, ICON_SAPMLE3};
+}
