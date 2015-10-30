83fb37a3ded65b22e2f3df41dd42a767e6c85fb3
David Becker <dasmau89@gmail.com>
2015-10-30 15:00:55 +0100

    show message for validation status

--- bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/dialog/AbstractDialog.java
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/dialog/AbstractDialog.java
@@ -1,8 +1,16 @@
 package de.afbb.bibo.ui.dialog;
 
+import org.eclipse.core.databinding.AggregateValidationStatus;
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.widgets.Shell;
 
@@ -12,6 +20,7 @@ import de.afbb.bibo.ui.observable.value.StatusToObservable;
 public abstract class AbstractDialog extends TitleAreaDialog {
 
 	protected final DataBindingContext bindingContext = new DataBindingContext();
+	private final IObservableValue validationStatus = new WritableValue(IStatus.OK, IStatus.class);
 
 	protected AbstractDialog(final Shell parentShell) {
 		super(parentShell);
@@ -28,8 +37,32 @@ public abstract class AbstractDialog extends TitleAreaDialog {
 	}
 
 	protected void aggregateStatus() {
-		bindingContext.bindValue(SWTObservables.observeEnabled(getButton(IDialogConstants.OK_ID)), new StatusToObservable(BindingHelper
-				.aggregateValidationStatus(bindingContext)), null, null);
+		final AggregateValidationStatus aggregateValidationStatus = BindingHelper.aggregateValidationStatus(bindingContext);
+		bindingContext.bindValue(validationStatus, aggregateValidationStatus, new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER),
+				new UpdateValueStrategy().setAfterConvertValidator(new IValidator() {
+
+					@Override
+					public IStatus validate(final Object value) {
+						final Status status = (Status) value;
+						switch (status.getSeverity()) {
+							case IStatus.OK:
+								setMessage("", IMessageProvider.NONE);//$NON-NLS-1$
+								break;
+							case IStatus.INFO:
+								setMessage(status.getMessage(), IMessageProvider.INFORMATION);
+								break;
+							case IStatus.WARNING:
+								setMessage(status.getMessage(), IMessageProvider.WARNING);
+								break;
+							case IStatus.ERROR:
+								setMessage(status.getMessage(), IMessageProvider.ERROR);
+								break;
+						}
+						return Status.OK_STATUS;
+					}
+				}));
+		bindingContext.bindValue(SWTObservables.observeEnabled(getButton(IDialogConstants.OK_ID)), new StatusToObservable(
+				aggregateValidationStatus), null, null);
 	}
 
 	protected abstract void createBinding();
