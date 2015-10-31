57a17e7efb85008e425a20cbf2e71e466f844077
David Becker <dasmau89@gmail.com>
2015-10-31 10:31:32 +0100

    javadoc

--- bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/Messages.java
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/Messages.java
@@ -1,5 +1,10 @@
 package de.afbb.bibo.ui;
 
+/**
+ * class to hold common messages
+ * 
+ * @author dbecker
+ */
 public final class Messages {
 
 	public static final String MSG_CONNECTION_ERROR = "Es besteht ein Verbindungs-Problem mit dem Server!";
