cb940c17f42000125438362e4b7deebe795a0533
David Becker <dasmau89@gmail.com>
2015-11-02 13:54:24 +0100

    stub implementation of super class of editor

--- bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/view/AbstractEditView.java
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/view/AbstractEditView.java
@@ -1,13 +1,42 @@
 package de.afbb.bibo.ui.view;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.part.EditorPart;
 
 abstract class AbstractEditView extends EditorPart {
 
+	private Image imageValidation;
+	private Label lblValidationImage;
+	private Label lblValidationMessage;
+
 	@Override
 	public void createPartControl(final Composite parent) {
+//		final ScrolledComposite scrolledComposite = new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+//		scrolledComposite.setLayout(new GridLayout(1, false));
+//
+//		final Composite wrapperComposite = new Composite(scrolledComposite, SWT.NONE);
+//		wrapperComposite.setLayout(new GridLayout(2, false));
+//		wrapperComposite.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+//		GridDataFactory.fillDefaults().grab(true, true).applyTo(wrapperComposite);
+//
+//		lblValidationImage = new Label(wrapperComposite, SWT.NONE);
+//		lblValidationMessage = new Label(wrapperComposite, SWT.NONE);
+//		lblValidationImage.setText("image");
+//		lblValidationMessage.setText("text");
+//
+//		final Composite content = new Composite(wrapperComposite, SWT.NONE);
+//		GridDataFactory.fillDefaults().grab(true, true).span(2, 1).applyTo(content);
+//
+//		initUi(content);
+//
+//		scrolledComposite.setContent(wrapperComposite);
+//		wrapperComposite.setSize(wrapperComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+//		scrolledComposite.setMinSize(wrapperComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+//		scrolledComposite.setExpandHorizontal(true);
+//		scrolledComposite.setExpandVertical(true);
 		initUi(parent);
 	}
 
