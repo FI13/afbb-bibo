0bf3b84121a1e5b99fa8749e761aa28816764941
David Becker <dasmau89@gmail.com>
2015-11-03 17:33:33 +0100

    WIP

--- bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/view/RegisterExemplarView.java
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/view/RegisterExemplarView.java
@@ -1,8 +1,12 @@
 package de.afbb.bibo.ui.view;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -13,6 +17,7 @@ import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 
+import de.afbb.bibo.share.model.Copy;
 import de.afbb.bibo.ui.Activator;
 
 /**
@@ -24,6 +29,8 @@ public class RegisterExemplarView extends AbstractEditView {
 
 	public static final String ID = "de.afbb.bibo.ui.registerexemplar";//$NON-NLS-1$
 	private static final String IMAGE_GROUP = "icons/16x16medien.png";//$NON-NLS-1$
+
+	private final Set<Set<Copy>> copies = new HashSet<Set<Copy>>();
 
 	/**
 	 * The text control that's displaying the content of the email message.
@@ -66,8 +73,17 @@ public class RegisterExemplarView extends AbstractEditView {
 		toolkit.createLabel(mediumGroup, "Typ");
 		toolkit.createText(mediumGroup, "Typ");
 
+		final Composite middle = toolkit.createComposite(top, SWT.NONE);
+		middle.setLayoutData(new GridData(SWT.CENTER, SWT.DEFAULT, true, false));
+		middle.setLayout(new GridLayout(2, false));
+		final Button btnToList = toolkit.createButton(middle, "In Liste übernehmen", SWT.NONE);
+		final Button btnToEdit = toolkit.createButton(middle, "In Beareitung übernehmen", SWT.NONE);
+
 		final Composite bottom = toolkit.createComposite(top, SWT.NONE);
 		bottom.setLayout(new GridLayout(2, false));
+		final GridData layoutDataMiddle = new GridData(SWT.CENTER, SWT.DEFAULT, true, false);
+		layoutDataMiddle.horizontalSpan = 2;
+		bottom.setLayoutData(layoutDataMiddle);
 		final Table copyTable = toolkit.createTable(bottom, SWT.NONE);
 		final Composite buttonComposite = toolkit.createComposite(bottom, SWT.NONE);
 		buttonComposite.setLayout(new GridLayout());
@@ -77,6 +93,9 @@ public class RegisterExemplarView extends AbstractEditView {
 
 		GridDataFactory.fillDefaults().applyTo(idGroup);
 		GridDataFactory.fillDefaults().applyTo(mediumGroup);
+//		GridDataFactory.fillDefaults().span(2, 1).grab(true, false).applyTo(middle);
+//		btnToList.setLayoutData(GridDataFactory.swtDefaults().align(SWT.END, SWT.DEFAULT).create());
+//		btnToEdit.setLayoutData(GridDataFactory.swtDefaults().align(SWT.BEGINNING, SWT.DEFAULT).create());
 		GridDataFactory.fillDefaults().span(2, 1).grab(true, true).applyTo(bottom);
 		GridDataFactory.swtDefaults().align(SWT.CENTER, SWT.TOP).applyTo(buttonComposite);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(copyTable);
