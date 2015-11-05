68d36eb52173c9ecfcfa402f2187b7e971c29211
David Becker <dasmau89@gmail.com>
2015-11-05 21:19:48 +0100

    add test fragment to share bundle

---
+++ bundles/de.afbb.bibo.share.test/src/test/java/de/afbb/bibo/share/test/CopyUtilTest.java
@@ -0,0 +1,15 @@
+package de.afbb.bibo.share.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CopyUtilTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
