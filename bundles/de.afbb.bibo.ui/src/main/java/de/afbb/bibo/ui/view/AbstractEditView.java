6de26fc9eb9a8a0bfe87fdcf603f221b565b57db
David Becker <dasmau89@googlemail.com>
2015-11-14 15:50:03 +0100

    display validation in edit view

--- bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/view/AbstractEditView.java
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/view/AbstractEditView.java
@@ -2,10 +2,24 @@ package de.afbb.bibo.ui.view;
 
 import java.net.ConnectException;
 
+import org.eclipse.core.databinding.AggregateValidationStatus;
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.internal.runtime.LocalizationUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
@@ -13,51 +27,36 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.part.EditorPart;
 
+import de.afbb.bibo.databinding.BindingHelper;
+
 abstract class AbstractEditView extends EditorPart {
 
 	protected static final String EMPTY_STRING = "";//$NON-NLS-1$
+	private static final String OK = LocalizationUtils.safeLocalize("ok");//$NON-NLS-1$
 
-	private Image imageValidation;
 	private Label lblValidationImage;
 	private Label lblValidationMessage;
+	private final IObservableValue validationStatus = new WritableValue(IStatus.OK, IStatus.class);
 	protected final DataBindingContext bindingContext = new DataBindingContext();
 	protected FormToolkit toolkit = new FormToolkit(Display.getCurrent());
 
 	@Override
 	public void createPartControl(final Composite parent) {
 		try {
-			// final ScrolledComposite scrolledComposite = new
-			// ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
-			// scrolledComposite.setLayout(new GridLayout(1, false));
-			//
-			// final Composite wrapperComposite = new
-			// Composite(scrolledComposite, SWT.NONE);
-			// wrapperComposite.setLayout(new GridLayout(2, false));
-			// wrapperComposite.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
-			// GridDataFactory.fillDefaults().grab(true,
-			// true).applyTo(wrapperComposite);
-			//
-			// lblValidationImage = new Label(wrapperComposite, SWT.NONE);
-			// lblValidationMessage = new Label(wrapperComposite, SWT.NONE);
-			// lblValidationImage.setText("image");
-			// lblValidationMessage.setText("text");
-			//
-			// final Composite content = new Composite(wrapperComposite,
-			// SWT.NONE);
-			// GridDataFactory.fillDefaults().grab(true, true).span(2,
-			// 1).applyTo(content);
-			//
-			// initUi(content);
-			//
-			// scrolledComposite.setContent(wrapperComposite);
-			// wrapperComposite.setSize(wrapperComposite.computeSize(SWT.DEFAULT,
-			// SWT.DEFAULT));
-			// scrolledComposite.setMinSize(wrapperComposite.computeSize(SWT.DEFAULT,
-			// SWT.DEFAULT));
-			// scrolledComposite.setExpandHorizontal(true);
-			// scrolledComposite.setExpandVertical(true);
-			initUi(parent);
-			initBinding();
+			final Composite outer = toolkit.createComposite(parent, SWT.NONE);
+			GridLayout layoutOuter = new GridLayout(1, false);
+			layoutOuter.marginHeight = layoutOuter.marginWidth = 0;
+			outer.setLayout(layoutOuter);
+			GridDataFactory.fillDefaults().grab(true, true).applyTo(outer);
+
+			Composite validationComposite = toolkit.createComposite(outer, SWT.NONE);
+			validationComposite.setLayout(new GridLayout(2, false));
+			lblValidationImage = toolkit.createLabel(validationComposite, EMPTY_STRING);
+			lblValidationMessage = toolkit.createLabel(validationComposite, EMPTY_STRING);
+			lblValidationMessage.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_RED));
+
+			initUi(outer);
+			createBinding();
 			additionalTasks();
 		} catch (ConnectException e) {
 			handle(e);
@@ -68,6 +67,57 @@ abstract class AbstractEditView extends EditorPart {
 	 * initializes the UX of the editor
 	 */
 	protected abstract void initUi(final Composite parent);
+
+	private void createBinding() throws ConnectException {
+		initBinding();
+		final AggregateValidationStatus aggregateValidationStatus = BindingHelper
+				.aggregateValidationStatus(bindingContext);
+		bindingContext.bindValue(validationStatus, aggregateValidationStatus,
+				new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER),
+				new UpdateValueStrategy().setAfterConvertValidator(new IValidator() {
+
+					@Override
+					public IStatus validate(final Object value) {
+						final Status status = (Status) value;
+						if (OK.equals(status.getMessage())) {
+							setMessage(EMPTY_STRING, null);
+						} else {
+							String newMessage = status.getMessage();
+							Image newImage = null;
+							if (newMessage != null) {
+								switch (status.getSeverity()) {
+								case IMessageProvider.NONE:
+									break;
+								case IMessageProvider.INFORMATION:
+									newImage = JFaceResources.getImage(Dialog.DLG_IMG_MESSAGE_INFO);
+									break;
+								case IMessageProvider.WARNING:
+									newImage = JFaceResources.getImage(Dialog.DLG_IMG_MESSAGE_WARNING);
+									break;
+								case IMessageProvider.ERROR:
+									newImage = JFaceResources.getImage(Dialog.DLG_IMG_MESSAGE_ERROR);
+									break;
+								}
+							}
+							setMessage(newMessage, newImage);
+						}
+						return Status.OK_STATUS;
+					}
+
+				}));
+	}
+
+	/**
+	 * sets validation message
+	 * 
+	 * @param message
+	 * @param image
+	 */
+	private void setMessage(String message, Image image) {
+		lblValidationImage.setImage(image);
+		lblValidationMessage.setText(message);
+
+	}
 
 	/**
 	 * initializes the databinding for the editor
@@ -97,6 +147,7 @@ abstract class AbstractEditView extends EditorPart {
 				MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Fehler",
 						"Es besteht ein Verbindungsfehler mit dem Server.\nDiese Ansicht wird sich nun schließen.");
 				getSite().getPage().closeEditor(AbstractEditView.this, false);
+				// TODO log error
 			}
 		});
 	}
