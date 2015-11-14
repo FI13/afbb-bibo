47020067d337015f8da6b38b171b46675f58aa55
David Becker <dasmau89@googlemail.com>
2015-11-14 17:07:28 +0100

    add empty stub implementation of copy service

---
+++ bundles/de.afbb.bibo.share/src/main/java/de/afbb/bibo/share/internal/stub/impl/CopyStubService.java
@@ -0,0 +1,46 @@
+package de.afbb.bibo.share.internal.stub.impl;
+
+import java.net.ConnectException;
+import java.util.Collection;
+
+import de.afbb.bibo.share.ICopyService;
+import de.afbb.bibo.share.model.Copy;
+import de.afbb.bibo.share.model.Medium;
+
+public class CopyStubService implements ICopyService {
+
+	@Override
+	public void update(Copy copy) throws ConnectException {		
+	}
+
+	@Override
+	public Collection<Copy> getGrouped(Integer id) throws ConnectException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void registerCopy(Collection<Copy> copies) throws ConnectException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<Copy> listAll() throws ConnectException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Copy> listCopies(Medium medium) throws ConnectException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Copy copy) throws ConnectException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
