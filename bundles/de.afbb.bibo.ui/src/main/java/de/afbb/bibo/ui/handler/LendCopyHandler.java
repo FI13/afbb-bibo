bdd5b5f1d008577988e130f3e8085847a4a606c1
David Becker <dasmau89@googlemail.com>
2015-11-15 19:08:18 +0100

    add event handler for lend event

---
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/handler/LendCopyHandler.java
@@ -0,0 +1,15 @@
+package de.afbb.bibo.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class LendCopyHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.err.println("LendCopyHandler");
+		return null;
+	}
+
+}
