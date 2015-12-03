240d000f2e7f01d37b1907769dcc83c4e3a98fa8
David Becker <dasmau89@googlemail.com>
2015-12-03 11:29:16 +0100

    create exclusion strategy for gson

---
+++ bundles/de.afbb.bibo.bibservlet.client/src/main/java/de/afbb/bibo/servletclient/connection/BeanExclusionStrategy.java
@@ -0,0 +1,28 @@
+package de.afbb.bibo.servletclient.connection;
+
+import java.beans.PropertyChangeSupport;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+
+/**
+ * {@link ExclusionStrategy} that allows {@link Gson} to serialize our model
+ * classes
+ *
+ * @author david
+ *
+ */
+public class BeanExclusionStrategy implements ExclusionStrategy {
+
+	@Override
+	public boolean shouldSkipField(final FieldAttributes arg0) {
+		return false;
+	}
+
+	@Override
+	public boolean shouldSkipClass(final Class<?> arg0) {
+		return PropertyChangeSupport.class.equals(arg0);
+	}
+
+}
