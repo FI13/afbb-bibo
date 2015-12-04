fc6e94aeeddbcbd74c13ef4df92bfab7a332e73a
David Becker <dasmau89@googlemail.com>
2015-12-04 12:39:41 +0100

    implement service call to fetch medium by ISBN

--- bundles/de.afbb.bibo.bibservlet.client/src/main/java/de/afbb/bibo/servletclient/connection/MediumServiceImpl.java
+++ bundles/de.afbb.bibo.bibservlet.client/src/main/java/de/afbb/bibo/servletclient/connection/MediumServiceImpl.java
@@ -2,7 +2,9 @@ package de.afbb.bibo.servletclient.connection;
 
 import java.net.ConnectException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import de.afbb.bibo.servletclient.ServiceLocator;
 import de.afbb.bibo.share.IMediumService;
 import de.afbb.bibo.share.beans.BeanExclusionStrategy;
 import de.afbb.bibo.share.callback.EventListener;
@@ -46,9 +49,22 @@ public class MediumServiceImpl implements IMediumService {
 
 	@Override
 	public Medium getMedium(final String isbn) throws ConnectException {
-		// TODO Auto-generated method stub
-		// currently never called
-		throw new UnsupportedOperationException("not implemented");
+		final Map<String, String> param = new HashMap<String, String>();
+		param.put("ISBN", isbn);
+		final HttpResponse resp = ServerConnection.getInstance().request("/stock/getMedium", "GET", param, null);
+		if (resp.getStatus() == HttpServletResponse.SC_OK) {
+			final Medium medium = gson.fromJson(resp.getData(), Medium.class);
+			if (medium != null) {
+				// we only get the id for type filled, so we need to fetch type
+				// separately
+				medium.setType(ServiceLocator.getInstance().getTypService().get(medium.getType().getId()));
+			}
+			return medium;
+		} else if (resp.getStatus() == HttpServletResponse.SC_NOT_FOUND) {
+			return null;
+		} else {
+			throw new ConnectException("Wrong status code. Recieved was: " + resp.getStatus());
+		}
 	}
 
 	@Override
