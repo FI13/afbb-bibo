fd47bad61109e41571fe6ce659133fee4fde65bf
David Becker <dasmau89@googlemail.com>
2015-12-10 19:12:56 +0100

    don't create exception when everything is ok

--- bundles/de.afbb.bibo.bibservlet.client/src/main/java/de/afbb/bibo/servletclient/connection/Utils.java
+++ bundles/de.afbb.bibo.bibservlet.client/src/main/java/de/afbb/bibo/servletclient/connection/Utils.java
@@ -35,6 +35,9 @@ final class Utils {
 	 * @throws ConnectException
 	 */
 	public static ConnectException createExceptionForCode(final int code) {
+		if (HttpServletResponse.SC_OK == code) {
+			return null;
+		}
 		if (HttpServletResponse.SC_UNAUTHORIZED == code) {
 			String sessionToken;
 			try {
