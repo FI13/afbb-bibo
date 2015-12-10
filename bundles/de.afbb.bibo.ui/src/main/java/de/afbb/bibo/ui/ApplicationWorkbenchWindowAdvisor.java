de1139ff3ff36b444f35787aa5dd19a852b65919
David Becker <dasmau89@googlemail.com>
2015-12-10 18:56:34 +0100

    show welcome view based on set flag

--- bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/ApplicationWorkbenchWindowAdvisor.java
+++ bundles/de.afbb.bibo.ui/src/main/java/de/afbb/bibo/ui/ApplicationWorkbenchWindowAdvisor.java
@@ -2,6 +2,8 @@ package de.afbb.bibo.ui;
 
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
@@ -10,6 +12,7 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 import de.afbb.bibo.share.SessionHolder;
 import de.afbb.bibo.ui.dialog.LoginDialog;
+import de.afbb.bibo.ui.view.WelcomeView;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -34,13 +37,20 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
 	@Override
 	public void postWindowOpen() {
-		final Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		final IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		final Shell shell = activeWorkbenchWindow.getShell();
 
 		new LoginDialog(shell).open();
 
-		if (shell != null && !shell.isDisposed()) {
+		if (shell != null && !shell.isDisposed() && SessionHolder.getInstance().getCurator() != null) {
 			shell.setText(TITLE + " - Angemeldet als: " + SessionHolder.getInstance().getCurator().getName());
-
+			if (SessionHolder.getInstance().getCurator().isShowWelcome()) {
+				try {
+					activeWorkbenchWindow.getActivePage().showView(WelcomeView.ID);
+				} catch (final PartInitException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
