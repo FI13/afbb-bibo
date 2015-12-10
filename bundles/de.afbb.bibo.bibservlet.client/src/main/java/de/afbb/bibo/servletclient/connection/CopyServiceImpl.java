f34bf0c4b2eaa3026904080e5ac58c95dc7b6f62
David Becker <dasmau89@googlemail.com>
2015-12-10 13:30:33 +0100

    avoid null pointer

--- bundles/de.afbb.bibo.bibservlet.client/src/main/java/de/afbb/bibo/servletclient/connection/CopyServiceImpl.java
+++ bundles/de.afbb.bibo.bibservlet.client/src/main/java/de/afbb/bibo/servletclient/connection/CopyServiceImpl.java
@@ -217,42 +217,45 @@ public class CopyServiceImpl implements ICopyService {
 	public Collection<Copy> listCopies(final Medium medium) throws ConnectException {
 		final Collection<Copy> result = new HashSet<Copy>();
 		final Map<String, String> param = new HashMap<String, String>();
-		param.put("id", medium.getMediumId().toString());
-		final HttpResponse resp = ServerConnection.getInstance().request("/stock/listCopies", "GET", param, null);
-		if (resp.getStatus() == HttpServletResponse.SC_OK) {
-			final String[] data = resp.getData().split("\n");
-			for (int i = 0; i < data.length; i++) {
-				final Copy copy = Utils.gson.fromJson(data[i], Copy.class);
-				if (copy != null) {
-					/*
-					 * we only get the id for sub-entities filled, so we need to
-					 * fetch them separately
-					 */
-					Borrower borrower = copy.getBorrower();
-					if (borrower != null) {
-						copy.setBorrower(ServiceLocator.getInstance().getBorrowerService().get(borrower.getId()));
+		if (medium != null && medium.getMediumId() != null) {
+			param.put("id", medium.getMediumId().toString());
+			final HttpResponse resp = ServerConnection.getInstance().request("/stock/listCopies", "GET", param, null);
+			if (resp.getStatus() == HttpServletResponse.SC_OK) {
+				final String[] data = resp.getData().split("\n");
+				for (int i = 0; i < data.length; i++) {
+					final Copy copy = Utils.gson.fromJson(data[i], Copy.class);
+					if (copy != null) {
+						/*
+						 * we only get the id for sub-entities filled, so we
+						 * need to fetch them separately
+						 */
+						Borrower borrower = copy.getBorrower();
+						if (borrower != null) {
+							copy.setBorrower(ServiceLocator.getInstance().getBorrowerService().get(borrower.getId()));
+						}
+						borrower = copy.getLastBorrower();
+						if (borrower != null) {
+							copy.setLastBorrower(
+									ServiceLocator.getInstance().getBorrowerService().get(borrower.getId()));
+						}
+						Curator curator = copy.getCurator();
+						if (curator != null) {
+							copy.setCurator(ServiceLocator.getInstance().getCuratorService().get(curator.getId()));
+						}
+						curator = copy.getLastCurator();
+						if (curator != null) {
+							copy.setLastCurator(ServiceLocator.getInstance().getCuratorService().get(curator.getId()));
+						}
+						copy.getMedium().setType(
+								ServiceLocator.getInstance().getTypService().get(copy.getMedium().getType().getId()));
+						result.add(copy);
 					}
-					borrower = copy.getLastBorrower();
-					if (borrower != null) {
-						copy.setLastBorrower(ServiceLocator.getInstance().getBorrowerService().get(borrower.getId()));
-					}
-					Curator curator = copy.getCurator();
-					if (curator != null) {
-						copy.setCurator(ServiceLocator.getInstance().getCuratorService().get(curator.getId()));
-					}
-					curator = copy.getLastCurator();
-					if (curator != null) {
-						copy.setLastCurator(ServiceLocator.getInstance().getCuratorService().get(curator.getId()));
-					}
-					copy.getMedium().setType(
-							ServiceLocator.getInstance().getTypService().get(copy.getMedium().getType().getId()));
-					result.add(copy);
 				}
-			}
-		} else {
-			final ConnectException exception = Utils.createExceptionForCode(resp.getStatus());
-			if (exception != null) {
-				throw exception;
+			} else {
+				final ConnectException exception = Utils.createExceptionForCode(resp.getStatus());
+				if (exception != null) {
+					throw exception;
+				}
 			}
 		}
 		return result;
